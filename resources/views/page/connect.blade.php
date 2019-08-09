76bfec16476dbe7b9d76f2fab1d5a9621f835952
BitaG <zhesikbilivol@gmail.com>
2019-08-09 16:22:52 +0300

    add welcome page

--- resources/views/page/connect.blade.php
+++ resources/views/page/connect.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('content')
-    <div id="app" class=" twocolordiv">
+    <div class="twocolordiv">
         <div id="app" class="container pt-5">
             <div class="row justify-content-center align-items-center" style="height: 80vh;">
                 <sp-connect
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+@section('content')
+    <div class="twocolordiv">
+        <div  class="container pt-5">
+            <div class="row justify-content-center align-items-center" style="height: 70vh;">
+                <div class="card col-md-8">
+                    <img src="{{asset('images/sendpulse-logo-color.svg')}}"  class="py-2">
+                    <img src="{{asset('images/bitrix24.svg')}}"  class="py-2">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
